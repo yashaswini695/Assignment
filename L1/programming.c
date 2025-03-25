35b57e26439c99e035bc70cc74a33ae4dd9f41bb
yashaswini695 <yashaswini.b@s.amity.edu>
2025-03-25 19:06:30 +0530

    second commit

---
+++ L1/programming.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ void main () {
+         int n;
+         printf("1.B.tech CSE\n2.Btech AIML");
+         if(n==1)
+         {
+                 printf("you have choosen B.tech CSE");
+         }
+	 if(n==2)
+	 {
+		 printf("you have choosen btech AIML");
+	 }
+
+ }
