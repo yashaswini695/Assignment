c8636be0d0d40a86239ada0c16460675800a6d8c
yashaswini695 <yashaswini.b@s.amity.edu>
2025-03-25 15:25:19 +0530

    C PROGRAM

---
+++ L8/programC.c
@@ -0,0 +1,11 @@
+#include <stdio>
+
+void main() {
+	int n;
+	printf("1.Computer Science Engineering");
+	printf("Enter the value of n: ");
+	if (n==1)
+	{
+		print("you have choosen Computer Science Engineering");
+	}
+}
