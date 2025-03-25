8ef9b41cca153294906d7de232e719240e6ff72e
yashaswini695 <yashaswini.b@s.amity.edu>
2025-03-25 21:20:33 +0530

    first commit

---
+++ ccode.c
@@ -0,0 +1,9 @@
+i#include <stdio.h>
+ void main () {
+         int n;
+         printf("1.B.tech CSE");
+         if(n==1)
+         {
+                 printf("you have choosen B.tech CSE");
+         }
+ }
